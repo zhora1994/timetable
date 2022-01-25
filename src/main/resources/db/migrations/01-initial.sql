31fa90b821eafa65c900eb872ea2faa1b26584c3
George <gosharustt@rambler.ru>
2022-01-26 00:02:15 +0300

    Change db_schema, add flyway and pom

---
+++ src/main/resources/db/migrations/01-initial.sql
@@ -0,0 +1,64 @@
+CREATE TABLE uzer
+(
+ login   varchar(50) PRIMARY KEY,
+ pwd_hash TEXT,
+ pwd_salt TEXT,
+ email varchar(50) NOT NULL,
+ name    varchar(50) NOT NULL,
+ surname varchar(50),
+ created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+ tg_id   bigint NOT NULL
+);
+
+CREATE TABLE uzer_2_club(
+    uzer_login varchar(50) NOT NULL,
+    club_name varchar(50) NOT NULL,
+    is_blocked BOOLEAN,
+    is_root BOOLEAN,
+    CONSTRAINT uzer_2_club_pk  PRIMARY KEY (uzer_login, club_name),
+    CONSTRAINT uzer_2_club_uzer_fk FOREIGN KEY uzer_login REFERENCES uzer(login),
+    CONSTRAINT uzer_2_club_club_fk FOREIGN KEY club_name REFERENCES club(name)
+);
+
+CREATE TABLE club
+(
+    name varchar(50) PRIMARY KEY,
+    address TEXT,
+    rttf_link TEXT,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    created_by TEXT NOT NULL,
+    deleted_at TIMESTAMP WITHOUT TIME ZONE,
+    deleted_by TEXT
+);
+
+
+CREATE TABLE time_slot
+(
+ id    int PRIMARY KEY,
+ time_start time NOT NULL,
+ time_end   time NOT NULL
+);
+
+CREATE TABLE table_info
+(
+ id    int PRIMARY KEY,
+ table_name varchar(10),
+ is_exists  boolean NOT NULL,
+ club_name varchar(50) NOT NULL,
+ CONSTRAINT table_info_club_fk FOREIGN KEY club_name REFERENCES club(name)
+);
+
+CREATE TABLE orders
+(
+ id           int NOT NULL,
+ table_name   varchar(10) NOT NULL,
+ slot_id      int NOT NULL,
+ login        varchar(50) NOT NULL,
+ ord_date         DATE NOT NULL,
+ is_avaliable boolean NOT NULL,
+ CONSTRAINT PK_50 PRIMARY KEY ( "id" ),
+ CONSTRAINT FK_63 FOREIGN KEY ( "table_name" ) REFERENCES table_info ( "table_name" ),
+ CONSTRAINT FK_66 FOREIGN KEY ( login ) REFERENCES "user" ( login ),
+ CONSTRAINT FK_74 FOREIGN KEY ( slot_id ) REFERENCES time_slot ( id ),
+ CONSTRAINT unq_key (table_name, ord_date, slot_id)
+);
